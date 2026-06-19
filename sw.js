f96bc5b8cbc59cfa1acc81f3e9f9417985dc93dc
bounce0208 <aruntgjames@gmail.com>
2026-06-19 16:36:18 +0100

    Design updates

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-const CACHE = 'slate-v3';
-const ASSETS = ['./slate.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).then(r => { const rc = r.clone(); caches.open(CACHE).then(c => c.put(e.request, rc)); return r; }).catch(() => caches.match(e.request))));
+var CACHE = 'slate-v2';
+var ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); }).then(function() { return self.skipWaiting(); }));
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }).then(function() { return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
